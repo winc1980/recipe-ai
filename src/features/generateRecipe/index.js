e9d350811e2f245d3897473fec7e1948cc4cf971
shuma1356 <capepalace1118@gmail.com>
2024-02-26 10:01:19 +0900

    レシピ生成 function calling

--- src/features/generateRecipe/index.js
+++ src/features/generateRecipe/index.js
@@ -2,14 +2,16 @@ import { openai } from '../openaiClient';
 
 // レシピを生成する
 export const generateRecipe = async () => {
+  console.log('レシピを生成中...');
   const chatCompletion = await openai.chat.completions.create({
+    model: 'gpt-4-0125-preview',
     messages: [
       {
         role: 'user',
         content: '',
       },
     ],
-    model: 'gpt-4-0125-preview',
+    
   });
   return chatCompletion.choices[0].message.content;
 };
