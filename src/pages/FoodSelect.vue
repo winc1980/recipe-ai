3cf133b58de61fdb790d4d58f886742ba64f9253
shuma1356 <capepalace1118@gmail.com>
2024-02-26 13:49:33 +0900

    foodselect.vue html繋ぎ

--- src/pages/FoodSelect.vue
+++ src/pages/FoodSelect.vue
@@ -1,3 +1,277 @@
-<script></script>
-<template>FoodSelect.vue</template>
-<style scoped></style>
+<script setup>
+import { ref } from 'vue';
+// import { detectFoods } from './features/detectFoods';
+// import { getImage } from './features/getImage';
+// import FoodSelect from './pages/FoodSelect.vue';
+// import Home from './pages/Home.vue';
+
+const searchQuery = ref(''); // 検索キーワード変数
+const thumbnailUrl = ref(''); // サムネイルURL変数
+
+// ページの表示非表示
+const foodSelectVisible = ref(false); // 食品選択の表示/非表示
+const homeVisible = ref(true); // ホームの表示/非表示
+
+// ページの切り替え
+const changePage = (pageName) => {
+  if (pageName === 'home') {
+    homeVisible.value = true;
+    foodSelectVisible.value = false;
+  }
+  if (pageName === 'foodSelect') {
+    homeVisible.value = true;
+    foodSelectVisible.value = true;
+  }
+};
+
+// サムネイルURLを取得する
+const getThumbnailUrl = async () => {
+  let url = await getImage(searchQuery.value);
+  console.log(url);
+  thumbnailUrl.value = url;
+};
+
+const imageUpload = (e) => {
+  changePage('foodSelect');
+  const files = e.target.files || e.dataTransfer.files;
+  const reader = new FileReader();
+  reader.onload = async (event) => {
+    const b64Image = event.currentTarget.result;
+    // 食材を検出する
+    const res = await detectFoods(b64Image);
+    console.log(res); // {foods_jp: array, foods_en: array}
+  };
+  reader.readAsDataURL(files[0]);
+};
+</script>
+<template>
+  <header>
+    <div class="select-title">
+      <img src="/arrow.png" alt="">
+      <span>食材選択</span>
+    </div>
+    
+  </header>
+  <main>
+    <div class="container">
+      <p>5品の食材を取得しました。</p>
+      <div>
+        <h1>食材を選択してください</h1>
+      </div>
+
+      <div>
+        <div class="select-image">
+            <li class="active"><img  src="/recipe1.png" alt=""><p>きゅうり</p></li>
+            <li><img src="/Milk_glass.png" alt=""><p>納豆</p></li>
+            <li><img src="/potato.png" alt=""><p>じゃがいも</p></li>
+        </div>
+        
+        <div class="select-image">
+            <li><img src="/milk.png" alt=""><p>牛乳</p></li>
+            <li><img src="/salt.png" alt=""><p>塩</p></li>
+            <li><img src="/400px-Table_salt_with_salt_shaker_V1.png" alt=""><p>チーズ</p></li>
+        </div>
+        
+      </div>  
+    </div>
+
+    <div class="list">
+        <div class="list-text">
+            <h2>追加で食材を入力</h2> 
+            <ul>
+                <li><p>豚のひき肉</p></li>
+                <li><p>しょうゆ</p></li>
+                <li><p>a</p></li>
+                <li><p>a</p></li>
+                <li><p>a</p></li>
+                <li><p>a</p></li>
+                <li><p>a</p></li>
+                <li><input type="text" placeholder="ここに食材を入力してください"></li>
+            </ul>
+        </div>
+        
+    </div>
+
+       
+
+  </main> 
+  <footer>
+    <div class="jump-button">
+        <button class="word-jump-button">
+          <span>決定する</span>
+        </button>
+      </div>
+  </footer>   
+</template>
+<style scoped>
+    header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100vw;
+    background-color: #FFFFFF;  
+    border-bottom: 1px solid #ECECEC; 
+    height: 65px;     
+    margin: auto;
+    padding-bottom: 7rem;
+    z-index: 3;
+
+}
+
+
+
+header img{
+    margin-top: 20px;
+}
+
+header span{
+    color: #442A00;
+    font-size: 18px;
+    font-weight: 700;
+    margin-left: 121px;
+}
+
+.select-title{
+    max-width: 353px;
+    margin: 0 auto;
+}
+
+.container{
+    max-width: 353px;
+    margin-top: 102px;
+    margin: 102px auto 0;
+
+}
+
+.container p{
+    margin-top: 26px;
+    color: #442A00;
+    font-weight: 500;
+    font-size: 15px;
+    margin-bottom: 23px;
+}
+
+h1{
+    color: #EF797B;
+    font-size: 18px;
+    text-align: center;
+    margin-bottom: 29px;
+}
+
+
+
+.select-image{
+    display: flex;
+    justify-content: space-around;
+}
+
+.select-image{
+    margin-bottom: 13px;
+}
+
+.select-image p{
+    color: #000000;
+    font-size: 15px;
+    font-weight: 700;
+    margin-bottom: 21px;
+    display: flex;
+    justify-content: space-around;
+}
+
+.select-image li{
+    list-style: none;
+}
+
+.select-image li img{
+    border-radius: 50%;
+    width: 106px;
+    height: 106px;
+    object-fit: cover;
+    border: 6px solid #9E9E9E;
+    padding: 3px;
+    opacity: 50%;
+}
+
+.select-image li.active img{
+    border-radius: 50%;
+    width: 106px;
+    height: 106px;
+    object-fit: cover;
+    border: 6px solid #EF797B;
+    padding: 3px;
+    opacity: 100%;
+}
+
+
+.list{
+    background-color: #FFF4F5;
+    width: 100vw;
+    padding-bottom: 32px;
+}
+
+.list-text{
+    max-width: 353px;
+    margin: 0 auto;
+    margin-bottom: 110px;
+
+
+}
+.list ul li{
+    list-style: none;
+    border-bottom: 1px solid #DED6CA;
+}
+
+.list p,.list input{
+    font-weight: 500;
+    padding-top: 17px;
+    padding-bottom: 15px;
+    margin-left: 12px;
+}
+
+.list input{
+    color: #DED6CA;
+
+
+}
+
+h2{
+    text-align: center;
+    color: #442A00;
+    font-size: 18px;
+    padding-top: 34px;
+    padding-bottom: 26px;
+    border-bottom: 1px solid #DED6CA;
+
+}
+
+.jump-button{
+    display: flex;
+    justify-content: center;
+    height: 110px;
+    
+
+}
+
+.word-jump-button{
+    background-color: #EF797B;
+    color: #FFFFFF;
+    width: 302px;
+    height: 45px;
+    border-radius: 23rem;
+    border: none;
+    font-size: 14px;
+    margin: 32px auto;
+    
+}
+
+footer{
+    border-top: 1px solid #ECECEC;
+    position:fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100vw;
+    background-color: #FFFFFF;
+}
+</style>
